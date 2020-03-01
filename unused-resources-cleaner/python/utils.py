cb1073670105ee15e8db4a8d271f7af41638aacf
Karolina Boboli <project@karolinaboboli.com>
2020-03-01 16:10:34 +0100

    Added listing of region names

---
+++ unused-resources-cleaner/python/utils.py
@@ -0,0 +1,10 @@
+import boto3
+import json
+
+def get_region_names(session):
+    ec2 = session.client('ec2', region_name='us-east-1')
+    regions = ec2.describe_regions()['Regions']
+    region_names = [region['RegionName'] for region in regions]
+    return region_names
+
+
