9fc4c185b78364f0c102e02ec392c10367bbb5a7
Karolina Boboli <project@karolinaboboli.com>
2020-03-01 18:18:58 +0100

    Added writing to CSV

--- unused-resources-cleaner/python/utils.py
+++ unused-resources-cleaner/python/utils.py
@@ -1,10 +1,16 @@
 import boto3
-import json
+import csv
 
 def get_region_names(session):
     ec2 = session.client('ec2', region_name='us-east-1')
     regions = ec2.describe_regions()['Regions']
     region_names = [region['RegionName'] for region in regions]
     return region_names
+
+def write_to_csv(report, file_path):
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(report['header'])
+        writer.writerows(report['rows'])
 
 
